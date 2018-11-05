f1c128c39ed7cca539141861d3b52994b414275e
Koyach <koya1@live.com>
2018-11-05 18:20:52 -0500

    continuation of package boilerplate

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,14 @@
-import 'package:dart_examples/dart_examples.dart' as dart_examples;
+import 'package:dart_examples/dart_examples.dart';
 
-main(List<String> arguments) {
-  print('Hello world: ${dart_examples.calculate()}!');
+void main() {
+  fizzBuzz();
+  print(reverseString("Hello Dart!"));
+  print(reverseWordsInString("The quick brown fox jumps over the lazy dog"));
+  print(reverseWordsInStringEfficient("The quick brown fox jumps over the lazy dog"));
+  print("---- ----");
+
+  benchmarkStringAndStringBuff("""
+  Lorem ipsum dolor amet banh mi viral locavore cardigan franzen whatever man bun cold-pressed meditation four loko street art put a bird on it. Fixie pickled beard street art raw denim. Single-origin coffee bushwick beard occupy, ennui actually meh schlitz vexillologist prism. Jean shorts shoreditch yr helvetica pabst flexitarian sriracha trust fund keytar cloud bread green juice. Microdosing semiotics keffiyeh kogi, air plant authentic coloring book organic pabst hashtag vexillologist seitan. Ugh post-ironic vexillologist austin cliche.
+  Spicy jalapeno bacon ipsum dolor amet aute voluptate tail, short loin pork belly eiusmod pastrami chuck ut ham in. Commodo ea pariatur minim fatback, biltong hamburger ground round spare ribs turducken officia. Occaecat kevin fugiat proident t-bone est pork loin shankle culpa pork fatback ut dolore. Hamburger mollit ribeye eiusmod, in do corned beef id. Beef ribs nostrud magna ham strip steak ut sed. Velit shoulder bacon boudin pancetta minim chicken do.
+  """);
 }
--- test/dart_examples_test.dart
+++ test/dart_examples_test.dart
@@ -2,7 +2,7 @@ import 'package:dart_examples/dart_examples.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('calculate42', () {
+    expect(calculate42(), 42);
   });
 }
